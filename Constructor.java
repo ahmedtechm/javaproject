ec358fce794c051285d84d45903c43a8cd6b70a5
ahmedtechm <ahmedtechm2050@gmail.com>
2022-12-25 04:47:06 -0800

    adding antother parameter to the constructor

--- Constructor.java
+++ Constructor.java
@@ -3,17 +3,24 @@ package school;
 public class Constructor {
 	
 	String studentName;
+	String familyName;
 	int studentAge;
-	public Constructor ( String name, int age) {
+	
+	
+	public Constructor ( String name,String fname, int age) {
 	    studentName = name;
+	    familyName = fname;
 	    studentAge = age;
 	  }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Constructor myobj = new Constructor("Ahmed" , "ALAbri",26);
+		System.out.println(myobj.studentName + "\n" + myobj.familyName+"\n "+myobj.studentAge);
 
-		Constructor myobj = new Constructor("Ahmed" , 27);
-		System.out.println(myobj.studentName + "\n" + myobj.studentAge);
+		
 
 	}
 
